c690ee3a8ea7429e46bcd477a80611a78f5578b0
Rodrigo Carneiro <rodrigo.carneiro@olxbr.com>
2017-07-04 03:32:21 -0300

    Return mongo for a standalone without user

--- web/config.py
+++ web/config.py
@@ -3,11 +3,10 @@ from pymongo import MongoClient
 
 WTF_CSRF_ENABLED = True
 SECRET_KEY = 'put_your_secret_key_here'
-DB_USER = str(os.environ.get('MONGODB_USERNAME'))
-DB_PASS = str(os.environ.get('MONGODB_PASSWORD'))
-DB_NAME = str(os.environ.get('MONGODB_DATABASE'))
 
-DATABASE = MongoClient("mongodb://{}:{}@mongodb:27017".format(DB_USER, DB_PASS))[DB_NAME]
+DB_NAME = 'tada'
+
+DATABASE = MongoClient("mongodb://mongodb:27017".format(DB_USER, DB_PASS))[DB_NAME]
 TODO_COLLECTION = DATABASE.todos
 USERS_COLLECTION = DATABASE.users
 SETTINGS_COLLECTION = DATABASE.settings
