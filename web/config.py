d8401257a8a9871fb8373816ec1d621504a5c971
Rodrigo Carneiro <rodrigo.carneiro@olxbr.com>
2017-07-04 02:47:47 -0300

    Casting env vars to string

--- web/config.py
+++ web/config.py
@@ -3,9 +3,9 @@ from pymongo import MongoClient
 
 WTF_CSRF_ENABLED = True
 SECRET_KEY = 'put_your_secret_key_here'
-DB_USER = os.environ.get('MONGO_USER')
-DB_PASS = os.environ.get('MONGO_PASSWORD')
-DB_NAME = os.environ.get('MONGO_DATABASE')
+DB_USER = str(os.environ.get('MONGO_USER'))
+DB_PASS = str(os.environ.get('MONGO_PASSWORD'))
+DB_NAME = str(os.environ.get('MONGO_DATABASE'))
 
 DATABASE = MongoClient("mongodb://{}:{}@mongodb:27017".format(DB_USER, DB_PASS))[DB_NAME]
 TODO_COLLECTION = DATABASE.todos
