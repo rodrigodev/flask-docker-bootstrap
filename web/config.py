1ee2b44b682632b685c0af5fb82c19d9ee6ae8ca
Rodrigo Carneiro <rodrigo.carneiro@olxbr.com>
2017-07-04 03:35:22 -0300

    Remove format

--- web/config.py
+++ web/config.py
@@ -6,7 +6,7 @@ SECRET_KEY = 'put_your_secret_key_here'
 
 DB_NAME = 'tada'
 
-DATABASE = MongoClient("mongodb://mongodb:27017".format(DB_USER, DB_PASS))[DB_NAME]
+DATABASE = MongoClient("mongodb://mongodb:27017")[DB_NAME]
 TODO_COLLECTION = DATABASE.todos
 USERS_COLLECTION = DATABASE.users
 SETTINGS_COLLECTION = DATABASE.settings
